cc8eadc28e625c12d299f562f5c1e46fd1987ba2
Ashish Kumar Verma <35832574+ashishverma4822@users.noreply.github.com>
2024-07-27 19:38:56 +0530

    Delete backend directory

--- backend/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { isAuthenticate } from "../middleware/verifyToken.js"
-import { getUserForSidebar } from "../controller/user.controller.js"
-
-const router = express.Router()
-
-router.get("/", isAuthenticate, getUserForSidebar)
-
-export default router
