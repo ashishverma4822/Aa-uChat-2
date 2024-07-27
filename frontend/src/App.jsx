9a89e65da1d6d6f37a3e724ef1636384a07dd54b
Ashish Kumar Verma <vashishk0602@gmail.com>
2024-07-27 17:54:10 +0530

    first-commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
+
+const App = () => {
+  const { authUser } = useAuthContext();
+
+  return (
+    <div className="p-4 h-screen flex items-center justify-center bg-custom-bg text-neon-blue">
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+      </Routes>
+      <Toaster
+        toastOptions={{
+          style: {
+            background: '#000000',
+            color: '#00FFFF',
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default App;
