a6505d14322be55bc98c405e50fb222442352260
Ashish Kumar Verma <35832574+ashishverma4822@users.noreply.github.com>
2024-07-28 15:30:58 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,29 +1,42 @@
-import { Server as SocketIO } from "socket.io";
-import http from "http";
-import express from "express";
+import { Server } from "socket.io"
+import http from "http"
+import express from "express"
 
-const app = express();
-const server = http.createServer(app);
+const app = express()
 
-const io = new SocketIO(server, {
-    cors: {
-        origin: "https://aa-u-chat-2.vercel.app/",
-        methods: ["GET", "POST"],
-    },
-});
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: ["https://aa-u-chat-2.vercel.app/"],
+    methods: ["GET", "POST"],
+  },
+})
 
 export const getReceiverSocketId = (receiverId) => {
-  return userSocketMap[receiverId];
-};
+  return userSocketMap[receiverId]
+}
 
-const userSocketMap = {};
+const userSocketMap = {}
 
 io.on("connection", (socket) => {
-  console.log("a user connected", socket.id);
+  console.log("a user connected", socket.id)
 
-  const userId = socket.handshake.query.userId;
+  const userId = socket.handshake.query.userId
 
   if (userId !== "undefined") {
-    userSocketMap[userId] = socket.id;
+    userSocketMap[userId] = socket.id
   }
-});
+
+  // io.emit  events send to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap))
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id)
+    delete userSocketMap[userId]
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+  })
+})
+
+export { app, server, io }
+
